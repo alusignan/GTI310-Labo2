695221c5b3bc241e49824a8b9ec5f66000a54f6a
Benozkleenex <am61420@ens.etsmtl.ca>
2015-10-09 13:02:19 -0400

    snr

--- AudioResampler/gti310/tp2/audio/SNRFilter.java
+++ AudioResampler/gti310/tp2/audio/SNRFilter.java
@@ -1,5 +1,8 @@
 package gti310.tp2.audio;
 
+import java.io.FileNotFoundException;
+
+import gti310.tp2.io.FileSink;
 import gti310.tp2.io.FileSource;
 
 public class SNRFilter implements AudioFilter {
@@ -11,12 +14,29 @@ public class SNRFilter implements AudioFilter {
 	private byte[] TotalDataSource, TotalDataModified;
 	private byte[] HeaderSource, HeaderModified;
 	
+	
+	
 	public void setSourceFile (FileSource fileSource) {
 		this.sourceFile = fileSource;
 	}
 	
 	public void setModifiedFile (FileSource fileSource) {
 		this.modifiedFile = fileSource;
+	}
+	
+	int byteArrayToInt(byte[] bytes) {
+	     return bytes[0]  | (bytes[1] & 0xFF) << 8 | (bytes[2] & 0xFF) << 16 | (bytes[3] & 0xFF << 24);
+	}
+	
+	public void InuputOutputFile(String modifiedLocation) {
+		try {
+			modifiedFile = new FileSource(modifiedLocation);
+			
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -27,6 +47,15 @@ public class SNRFilter implements AudioFilter {
 		HeaderModified = sourceFile.pop(40);
 		
 		/*Lire les bits 40-44 pour savoir cmb de temps loop*/
+		
+		 for (int i = 1; i <= 04; i--){
+			 int source;
+			 int TotalInt = 0;
+			 
+			 source = byteArrayToInt(sourceFile.pop(1));
+			 TotalInt= source+TotalInt;
+			 System.out.println("allo"+TotalInt);
+		 }
 		
 		/*Loop sur les bits 44 et + pour calculer l'échantillion*/
 	}
