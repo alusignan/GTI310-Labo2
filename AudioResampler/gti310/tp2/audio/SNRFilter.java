7e8e6054912d4a0a533a98a0804fa26cd352308b
alusignan <am08390@ens.etsmtl.ca>
2015-10-09 10:21:10 -0400

    Trololol

--- AudioResampler/gti310/tp2/audio/SNRFilter.java
+++ AudioResampler/gti310/tp2/audio/SNRFilter.java
@@ -7,6 +7,9 @@ public class SNRFilter implements AudioFilter {
 	private FileSource modifiedFile;
 	private FileSource sourceFile;
 	private double[] SNRArray;
+	private byte[] DataArraySource, DataArrayModified;
+	private byte[] TotalDataSource, TotalDataModified;
+	private byte[] HeaderSource, HeaderModified;
 	
 	public void setSourceFile (FileSource fileSource) {
 		this.sourceFile = fileSource;
@@ -16,13 +19,16 @@ public class SNRFilter implements AudioFilter {
 		this.modifiedFile = fileSource;
 	}
 	
-	
-	
 	@Override
 	public void process() {
 		
+		//Skip les 2 headers
+		HeaderSource = sourceFile.pop(40);
+		HeaderModified = sourceFile.pop(40);
 		
+		/*Lire les bits 40-44 pour savoir cmb de temps loop*/
 		
+		/*Loop sur les bits 44 et + pour calculer l'échantillion*/
 	}
 
 }
