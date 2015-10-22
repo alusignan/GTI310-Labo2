d091b6fc6134511835581b8772dbb5f53301868f
Benozkleenex <am61420@ens.etsmtl.ca>
2015-10-22 17:42:57 -0400

    excelsior

--- AudioResampler/gti310/tp2/Application.java
+++ AudioResampler/gti310/tp2/Application.java
@@ -22,8 +22,10 @@ public class Application {
 			if (args[0].equals("Programme2")) {
 				System.out.println("SNR project!");
 				SNRFilter snr = new SNRFilter();
-				snr.InuputOutputFile(args[1],args[2]);
+				for(int j=2; j<10; j++){
+				snr.InuputOutputFile(args[1],args[j]);//,args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10]);
 				snr.process();
+				}
 		}
 		else {
 			System.out.println("Wut!?");
--- AudioResampler/gti310/tp2/audio/SNRFilter.java
+++ AudioResampler/gti310/tp2/audio/SNRFilter.java
@@ -3,25 +3,35 @@ package gti310.tp2.audio;
 import java.io.FileNotFoundException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import gti310.tp2.io.FileSink;
 import gti310.tp2.io.FileSource;
 
 public class SNRFilter implements AudioFilter {
-
+// variable pour les fichiers a comparer
 	private FileSource modifiedFile;
+// variable du fichier original	
 	private FileSource sourceFile;
+//Data après le pop de 44 	
 	private short[] dataOri;
 	private double[] SNRArray;
 	private byte[] DataArraySource, DataArrayModified;
 	private byte[] TotalDataSource, TotalDataModified;
 	private byte[] HeaderSource, HeaderModified;
-	short calcSNR;
-	short calcTotTop;
-	short calcTotBot;
-	short calcTop;
-	short calcBot;
-	short calcBot2;
+// variable du calcul final	
+	double calcSNR;
+	long calcTotTopFin;
+	long calcTotTop;
+	long calcTotBot;
+	long calcTop;
+	long calcBot;
+	long calcBot2;
+//la liste ou sera stocker les valeurs a comparer	
+    List<Double> SNRtbl = new ArrayList<Double>(8);
 	
 	
 	
@@ -29,16 +39,15 @@ public class SNRFilter implements AudioFilter {
 		this.sourceFile = fileSource;
 	}
 	
-	public void setModifiedFile (FileSource fileSource) {
-		this.modifiedFile = fileSource;
-	}
+//premier test pour essayer de convertir les données hexadécimal en entier	
 	
-	int byteArrayToInt(byte[] bytes) {
-	     return ((bytes[3] & 0xFF >> 24) |  (bytes[2] & 0xFF >> 16) |(bytes[1] & 0xFF >> 8) | (bytes[0] & 0xFF) );
-		 //return bytes[0] | (bytes[1] & 0xFF) << 8 | (bytes[2] & 0xFF) << 16 | (bytes[3] & 0xFF << 24);
-	}
+//	int byteArrayToInt(byte[] bytes) {
+//	     return ((bytes[3] & 0xFF >> 24) |  (bytes[2] & 0xFF >> 16) |(bytes[1] & 0xFF >> 8) | (bytes[0] & 0xFF) );
+//       return bytes[0] | (bytes[1] & 0xFF) << 8 | (bytes[2] & 0xFF) << 16 | (bytes[3] & 0xFF << 24);
+//	}
 	
-	public void InuputOutputFile(String sourceLocation, String modifiedLocation) {
+//lecture des fichiers originaux	
+	public void InuputOutputFile(String sourceLocation, String modifiedLocation){ //, String F3, String F4, String F5, String F6, String F7, String F8, String F9, String F0) {
 		try {
 			sourceFile = new FileSource(sourceLocation);
 			modifiedFile = new FileSource(modifiedLocation);
@@ -50,52 +59,57 @@ public class SNRFilter implements AudioFilter {
 		}
 	}
 	
+//fonction principale	
 	@Override
 	public void process() {
+		int t=0;
 		
-		//Skip les 2 headers
+		//Skip le header source
 		HeaderSource = sourceFile.pop(40);
-		HeaderModified = modifiedFile.pop(40);
 		
-		/*Lire les bits 40-44 pour savoir cmb de temps loop*/
-		
-		//Plus une boucle avec celui là je crois
+//variable qui contient le hexa pour savoir combien il y a de boucle a faire		
 		TotalDataSource = sourceFile.pop(4);
-		//int myDataSizeSource = byteArrayToInt(TotalDataSource);
+		
+		// le pop necessaire pour le fichier a comparé et aller directement aux data
+		TotalDataModified = modifiedFile.pop(4);
+		HeaderModified = modifiedFile.pop(40);
+// la variable qui permet de prendre le hexa et le mettre en entier		
 		int myDataSizeSource = ByteBuffer.wrap(TotalDataSource).order(ByteOrder.LITTLE_ENDIAN).getInt();
 		
-		//Pas sur que tu as besoin de boucler avec celui là
-		TotalDataModified = modifiedFile.pop(4);
-		int myDataSizeModified = ByteBuffer.wrap(TotalDataModified).order(ByteOrder.LITTLE_ENDIAN).getInt();
-		
 		System.out.println(myDataSizeSource);
-		System.out.println(myDataSizeModified);
 		
 		for(int i=0; i<myDataSizeSource; i++){
 			
-			short dataOri2 = 0;
-			short calcBot = 0;
-			short calcBot2 = 0;
+			long dataOri2 = 0;
+			long calcBot = 0;
+			long calcBot2 = 0;
 			
-			//dataOri[i] = (short) (sourceFile.pop(1)[0] & 0xff);
-			short dataOri =  (short) (modifiedFile.pop(1)[0] & 0xff);
-			short dataModif =  (short) (modifiedFile.pop(1)[0] & 0xff);
+//permet de sortir les valeurs une par une pour pouvoir les comparées
+			long dataOri =  (long) (sourceFile.pop(1)[0] & 0xff);
+			long dataModif =  (long) (modifiedFile.pop(1)[0] & 0xff);
 			
-			dataOri2 = (short) (dataOri*dataOri);
+//Tout le calculs qui permettre d'avoir la valeur du SNR			
+			dataOri2 = (long) (dataOri*dataOri);
 			
-			calcTop += (short)  dataOri2;
-			calcBot = (short) (dataOri-dataModif);
-			calcBot2 = (short) (calcBot*calcBot);
-			calcTotTop += (short) (calcTop);
-			calcTotBot += (short) (calcBot2);
-			System.out.println(i);
+			calcBot = (long) (dataOri-dataModif);
+			calcBot2 = (long) (calcBot*calcBot);
+			calcTotTop += (long) (dataOri2);
+			calcTotBot += (long) (calcBot2);
 			
 		}
-		System.out.println(calcTotTop);
-		System.out.println(calcTotBot);
-		calcSNR = (short) (10*Math.log10(calcTotTop/calcTotBot));
-		System.out.println(calcSNR);
+//calcul final avec les valeurs plus haut
+		calcSNR = (double) (10*Math.log10(calcTotTop/calcTotBot));
+//ajout de la valeur d'un calcul à la liste
+//Calcule bon individuellement mais a plusieurs mauvais résultat		
+		SNRtbl.add(calcSNR);
+//Trie des résultats		
+		for (int i=0; i<SNRtbl.size();i++){
+			Collections.sort(SNRtbl);
+			System.out.println((SNRtbl.get(i)));
+		}
+		
 		/*Loop sur les bits 44 et + pour calculer l'échantillion*/
 	}
+
 
 }
